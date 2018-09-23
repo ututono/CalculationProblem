408234fc63cb2fa46cbe1e026de89cc040a2c6dc
utuname <32908119+ututono@users.noreply.github.com>
2018-09-23 16:30:30 +0800

    write Blog and dos can be used in CMD.

--- CalculationProblem/CalculationProblemCreater/Funktion.h
+++ CalculationProblem/CalculationProblemCreater/Funktion.h
@@ -9,17 +9,6 @@ struct Variable
 	int size = 0;
 };
 
-struct VarSq
-{
-	char *elem;
-	int front;
-	int rear;
-	int maxSize;
-};
-
-//队列操作
-//int InitStack_Sq(VarSq &S, int size, int inc);
-
 char OperationalCreater();				//	运算符生成函数
 void NumCreater(int r, struct Variable m_VarStruct[], int x);	// 变量生成函数，r为范围参数，x为结构体数组编号
 int Creater(int r);
